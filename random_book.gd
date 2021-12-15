70eefc7c4a318fa128ab12cc90521214d31ea4a2
RexTheWho <rexjrc1@gmail.com>
2021-12-15 23:24:22 +0800

    randomizer button + improvements

---
+++ random_book.gd
@@ -0,0 +1,138 @@
+extends Node
+
+const COLOR_COLORS = [
+	Color.aqua,
+	Color.rebeccapurple,
+	Color.chartreuse,
+	Color.crimson,
+	Color.darkorange,
+	Color.deeppink,
+	Color.forestgreen,
+	Color.gold,
+	Color.lightseagreen,
+	Color.royalblue,
+	Color.yellowgreen,
+]
+
+const COVER_COLORS = []
+const COVER = [
+	preload("res://random_book/book_cover.png"),
+#	preload("res://random_book/book_cover2.png"),
+#	preload("res://random_book/book_cover3.png"),
+]
+const PAPER = preload("res://random_book/paper.png")
+const BOW = [
+	preload("res://random_book/bow.png"),
+	preload("res://random_book/bow2.png"),
+	preload("res://random_book/bow3.png"),
+]
+const TEXTS = [
+	preload("res://random_book/text1.png"),
+	preload("res://random_book/text2.png"),
+	preload("res://random_book/text3.png"),
+	preload("res://random_book/text4.png"),
+]
+
+var book_image:Image
+
+
+func _ready():
+	_debug_get_used_colors( PAPER.get_data() )
+
+
+func generate_book() -> Image:
+	book_image = Image.new()
+	
+	book_image.create(16, 16, false, Image.FORMAT_RGBA8)
+	
+	#
+	var cover:Image = COVER[randi()%COVER.size()].get_data()
+	_draw_layer(16, cover, book_image, 3.0)
+	
+	#
+	var paper:Image = PAPER.get_data()
+	_draw_layer(16, paper, book_image, 1.0,
+		Color.white,
+		Color.white,
+		Color.white
+	)
+		
+	#
+	if randi()%100 > 33:
+		var text:Image = TEXTS[randi()%TEXTS.size()].get_data()
+		_draw_layer(16, text, book_image, 1.0,
+			Color(0.1, 0.1, 0.1, 0.3),
+			Color(0.1, 0.1, 0.1, 0.3),
+			Color(0.1, 0.1, 0.1, 0.3)
+		)
+	
+	#
+	if randi()%100 > 33:
+		var bow:Image = BOW[randi()%BOW.size()].get_data()
+		_draw_layer(16, bow, book_image, 1.0)
+	
+	return book_image
+
+const CRS = 0.7
+const CRE = 0.98
+func _draw_layer(res:int, from_image:Image, to_image:Image, lightness:float = 1.0, tint_color1:Color = Color.black, tint_color2:Color = Color.black, tint_color3:Color = Color.black):
+	if tint_color1 == Color.black:
+		tint_color1 = rand_color()
+		
+	if tint_color2 == Color.black:
+		tint_color2 = rand_color()
+		
+	if tint_color3 == Color.black:
+		tint_color3 = rand_color()
+	
+	for x in res:
+		for y in res:
+			from_image.lock()
+			var col:Color = from_image.get_pixel(x,y)
+			from_image.unlock()
+			
+			if col.a == 0: # Skip to visible pixel...
+				continue
+			
+			col *= lightness
+			
+			if col.r > 0.0: # Uses Tint1
+				if tint_color1.a >= 1.0:
+					col = Color(col.r,col.r,col.r) * tint_color1
+				else:
+					col = Color(col.r,col.r,col.r).linear_interpolate(Color(col.r,col.r,col.r) * tint_color1, tint_color1.a)
+			
+			elif col.g > 0.0: # Uses Tint2
+				if tint_color2.a >= 1.0:
+					col = Color(col.g,col.g,col.g) * tint_color2
+				else:
+					col = Color(col.g,col.g,col.g).linear_interpolate(Color(col.g,col.g,col.g) * tint_color2, tint_color2.a)
+			
+			elif col.b > 0.0: # Uses Tint3
+				if tint_color3.a >= 1.0:
+					col = Color(col.b,col.b,col.b) * tint_color3
+				else:
+					col = Color(col.b,col.b,col.b).linear_interpolate(Color(col.b,col.b,col.b) * tint_color3, tint_color3.a)
+			
+			else: # Just black
+				col = Color.black
+			
+			to_image.lock()
+			to_image.set_pixel(x,y,col)
+			to_image.unlock()
+
+
+
+func _debug_get_used_colors(image:Image):
+	var used_colors = []
+	for x in image.data.width:
+		for y in image.data.height:
+			image.lock()
+			var col:Color = image.get_pixel(x,y)
+			if !used_colors.has(col):
+				used_colors.append(col)
+			image.unlock()
+
+
+func rand_color() -> Color:
+	return COLOR_COLORS[randi() % COLOR_COLORS.size()].contrasted()
--- root.gd
+++ root.gd
@@ -27,29 +27,34 @@ const PROPTEMP_level =		"enchantmentLevels=%s"
 
 
 export(NodePath) var mod_id
+export(NodePath) var enchant_id
 export(NodePath) var book_button
 export(NodePath) var step_by_step_label
 export(NodePath) var warning_label
 
 
-var book_id:String = "testingness"
+var book_id:String = "default_name"
 var book_level:int = 0
-var book_image:Image
+onready var book_image:Image = Image.new()
 
 
 func _ready():
+	randomize()
 	prints("OUTPUTTING TO:", resource_pack_path)
 	get_node(warning_label).text = "!! OUTPUT WARNING !!\n" + resource_pack_path + ".."
 	OS.center_window()
-	OS.set_window_title("Bookie")
+	if randi()%100 == 0:
+		OS.set_window_title("Bowokie")
+	else:
+		OS.set_window_title("Bookie")
 	get_tree().connect("files_dropped", self, "_files_dropped") # warning-ignore:return_value_discarded
+	book_image.load("res://default.png")
 
 
 func _files_dropped(files:PoolStringArray, _screen:int):
 	var path:String = files[0]
 	if path.get_extension() == "png":
-		get_node(step_by_step_label).text = "Now press the book to output!"
-		set_bookbutton_sprite(path)
+		set_bookbutton_sprite_from_path(path)
 		var sprite = path.right(path.find_last("\\")+1).trim_suffix(".png")
 		book_id = ""
 		
@@ -62,30 +67,41 @@ func _files_dropped(files:PoolStringArray, _screen:int):
 				book_level = num
 			else:
 				book_id += l
-		print(get_enchantment_and_mod())
 	
 	# not valid sprite...
 	else:
 		push_warning("Failed to build book, this is not a PNG file!")
 
 
-func set_bookbutton_sprite(path:String):
+func set_bookbutton_sprite_from_path(path:String):
 	book_image = Image.new()
 	if book_image.load(path) != OK:
 		book_image.lock()
 		book_image.fill(Color.purple)
 		book_image.unlock()
 		push_warning("Image could not be loaded, Enjoy purple!")
+	_enable_bookbutton()
+
+
+func set_bookbutton_sprite_from_image(image:Image):
+	book_image = image
+	_enable_bookbutton()
+
+
+func _enable_bookbutton():
+	var texbutt = get_node(book_button)
 	
 	var book_texture:ImageTexture = ImageTexture.new()
-	book_texture.create_from_image(book_image,0)
-	var texbutt = get_node(book_button)
-	texbutt.texture_normal = book_texture
-	texbutt.texture_pressed = book_texture
-	texbutt.texture_hover = book_texture
-	texbutt.texture_focused = book_texture
+	if book_image:
+		book_texture.create_from_image(book_image,0)
+		texbutt.texture_normal = book_texture
+		texbutt.texture_pressed = book_texture
+		texbutt.texture_hover = book_texture
+		texbutt.texture_focused = book_texture
+		
 	texbutt.self_modulate = Color.white
 	texbutt.disabled = false
+	get_node(step_by_step_label).text = "Now press the book to output!"
 
 
 func _on_bookbutton_pressed():
@@ -124,7 +140,7 @@ func save_book_property():
 	var stuff:String = ""
 	stuff += PROPTEMP_type + "\n"
 	stuff += PROPTEMP_item + "\n"
-	stuff += PROPTEMP_texture % get_enchantment_and_level() + "\n"
+	stuff += PROPTEMP_texture % get_enchant_texture_id() + "\n"
 	stuff += PROPTEMP_enchant % get_enchantment_and_mod() + "\n"
 	if book_level > 0:
 		stuff += PROPTEMP_level % book_level + "\n"
@@ -141,28 +157,49 @@ func save_book_property():
 func save_book_texture():
 	print("Saving Texture!")
 	check_and_correct_directory(get_output_texture())
-	var save_path = "%s/%s%s"%[get_output_texture(), get_enchantment_and_level(), ".png"]
+	var save_path = "%s/%s%s"%[get_output_texture(), get_enchant_texture_id(), ".png"]
+	if book_image == null:
+		book_image = Image.new()
+		book_image.load("res://error.png")
 	if book_image.save_png(save_path) != OK:
 		push_warning("Failed to save Texture!")
 
 
 func get_enchantment_and_level() -> String:
-	var string = book_id
+	var string = get_book_id()
 	if book_level > 0: string += str(book_level)
 	return string
 
 
 func get_enchantment_and_mod() -> String:
-	var string = book_id
+	var string = get_book_id()
 	var mod_name = get_node(mod_id).text
 	if mod_name != "":
 		string = mod_name + ":" + string
 	return string
 
 
+func get_enchant_texture_id() -> String:
+	var string = get_book_id()
+	var mod_name = get_node(mod_id).text
+	if mod_name != "":
+		string = mod_name + "_" + string
+	if book_level > 0:
+		string += str(book_level)
+	return string
+
+
 func get_mod_id() -> String:
 	var modid = get_node(mod_id)
 	return modid.text
+
+
+func get_book_id() -> String:
+	var book_it_return = book_id
+	var enchant_ovr = get_node(enchant_id)
+	if enchant_ovr.text != "":
+		book_it_return = enchant_ovr.text
+	return book_it_return
 
 
 func get_output_model() -> String:
@@ -179,4 +216,18 @@ func get_output_texture() -> String:
 
 func check_and_correct_directory(path):
 	var dir:Directory = Directory.new()
-	dir.make_dir_recursive(path)
+	dir.make_dir_recursive(path) # warning-ignore:return_value_discarded
+
+
+func _on_random_book_pressed():
+	var book = $RandomBookGenerator.generate_book()
+	set_bookbutton_sprite_from_image(book)
+
+
+# Can change this later, small UI stuff
+var _on_EnchantmentOverride_text_entered_once = true
+func _on_EnchantmentOverride_text_entered_once(_t):
+	if _on_EnchantmentOverride_text_entered_once == true:
+		print("_on_EnchantmentOverride_text_entered_once")
+		_on_EnchantmentOverride_text_entered_once = false
+		_enable_bookbutton()
